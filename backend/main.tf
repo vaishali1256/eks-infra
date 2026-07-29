965aaac45324d925960c87f4f0e3ef55bd215def
Vaishali Sharma <vaishali@Prakhars-MacBook-Air.local>
2026-07-28 20:16:25 -0400

    eks-infra-backend

---
+++ backend/main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "terraform-eks-state-bucket"
+
+  lifecycle {
+    prevent_destroy = false
+  }
+}
+
+resource "aws_dynamodb_table" "example" {
+  name         = "terraform-eks-state-lock"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+}
